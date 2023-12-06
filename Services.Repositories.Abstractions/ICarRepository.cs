29a4a056f50e72372bd32e2d12a3d989d50f95e0
maryia.paliashchuk@gmail.com <maryia.paliashchuk@gmail.com>
2023-12-06 18:45:52 +0300

    CRUD for CarService

---
+++ Services.Repositories.Abstractions/ICarRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Repositories.Abstractions
+{
+    public interface ICarRepository : IRepository<Car, int>
+    {
+        Task<IEnumerable<Car>> GetCars();
+        Task<int> AddCar(Car car);
+        Task<bool> DeleteCar(int id);
+        Task UpdateCar(Car car);
+    }
+}
