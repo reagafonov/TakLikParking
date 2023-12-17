e8a9f5dbc9c07ef405ff85719fc6ea64a7ae24f5
Максим Вебер <m.weber@samolet.ru>
2023-12-17 23:06:39 +0700

    role service

---
+++ Infrastructure.Repositories.Implementations/RoleRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Services.Repositories.Abstractions;
+
+namespace Infrastructure.Repositories.Implementations;
+public class RoleRepository : Repository<Role, int>, IRoleRepository
+{
+    public RoleRepository(DbContext context) : base(context)
+    {
+    }
+
+    public async Task<ICollection<Role>> GetPagedAsync(int page, int pageSize, CancellationToken token)
+    {
+        return await GetAll().OrderBy(x => x.Id).Skip((page - 1) * pageSize).Take(pageSize).ToListAsync(token);
+    }
+}
---
+++ Services.Repositories.Abstractions/IRoleRepository.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Entities;
+
+namespace Services.Repositories.Abstractions;
+
+public interface IRoleRepository : IRepository<Role, int>
+{
+    Task<ICollection<Role>> GetPagedAsync(int page, int pageSize, CancellationToken token);
+}
