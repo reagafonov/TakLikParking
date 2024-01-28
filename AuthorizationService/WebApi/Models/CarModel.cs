9cf9f6c4eba5bbf9535887706f4f873577b44dd0
Максим Вебер <m.weber@samolet.ru>
2024-01-28 19:44:39 +0700

    добавил удаленные файлы

---
+++ AuthorizationService/WebApi/Models/CarModel.cs
@@ -0,0 +1,11 @@
+using Services.Contracts;
+
+namespace WebApi.Models
+{
+    public class CarModel
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string Number { get; set; }
+    }
+}
